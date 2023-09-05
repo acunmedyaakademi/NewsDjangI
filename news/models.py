b16b0f8f54601c23a19d14a2ac4909e4690595ac
İbrahim Kahraman <ikahraman1938@gmail.com>
2023-09-05 14:39:44 +0300

    veritabanı modeller olusturuldu

--- news/models.py
+++ news/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class newsCategory(models.Model):
+     name = models.CharField(max_length=120)
+     class Meta:
+         db_table = 'NewsCategories'
+         verbose_name = 'Haber Kategorisi'
+         verbose_name_plural = 'Haber Kategorileri'
+
+     def __str__(self):
+         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(max_length=120)
+    summary = models.TextField(max_length=300)
+    contents = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    update_on = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(newsCategory, on_delete=models.CASCADE, related_name='news')
+
+    class Meta:
+        db_table = 'News'
+        verbose_name = 'New'
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return self.title
+
+
